3aef1bd1089dd4537dd64ddb7dca980e93d6d429
MelihOzdemir <canmelihozdemir@gmail.com>
2021-05-14 13:48:40 +0300

    + added ObjectPoolDesignPattern

---
+++ Assets/Scripts/2-ObjectPoolDesignPattern/ObjectPoolDesignPattern.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DesignPatterns.ObjectPool
+{
+    public class ObjectPoolDesignPattern : MonoBehaviour
+    {
+        [Serializable] public struct Pool
+        {
+            public int poolSize;
+            public GameObject poolObject;
+            public Queue<GameObject> poolList;
+        }
+        [SerializeField] private Pool[] _pool=null;
+        //[SerializeField] private int _poolSize;
+        //[SerializeField] private GameObject _poolObject;
+        //private Queue<GameObject> _poolList;
+
+        private void Awake()
+        {
+            for (int i1 = 0; i1  < _pool.Length; i1++)
+            {
+                _pool[i1].poolList = new Queue<GameObject>();
+
+                for (int i2 = 0; i2 < _pool[i1].poolSize; i2++)
+                {
+                    GameObject gameObject = Instantiate(_pool[i1].poolObject);
+                    gameObject.name = _pool[i1].poolObject.name + (i2 + 1).ToString();
+                    gameObject.SetActive(false);
+                    _pool[i1].poolList.Enqueue(gameObject);
+                }
+            }
+        }
+
+        public GameObject GetPoolObject(int objectType)
+        {
+            if (objectType>=_pool.Length || objectType<0)
+            {
+                return null;
+            }
+
+            GameObject gameObject = _pool[objectType].poolList.Dequeue();
+            gameObject.SetActive(true);
+            _pool[objectType].poolList.Enqueue(gameObject);
+
+            return gameObject;
+        }
+
+        public int GetPoolLengthOfType()
+        {
+            return _pool.Length;
+        }
+
+    }
+
+}
---
+++ Assets/Scripts/2-ObjectPoolDesignPattern/ObjectPoolSpawner.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DesignPatterns.ObjectPool
+{
+    public class ObjectPoolSpawner : MonoBehaviour
+    {
+        [SerializeField] private float _spawnInterval = 1f;
+        private ObjectPoolDesignPattern _objectPool;
+
+        private void Awake()
+        {
+            _objectPool = GetComponent<ObjectPoolDesignPattern>();
+        }
+
+        private void Start()
+        {
+            StartCoroutine(nameof(SpawnRoutine));
+        }
+
+        private IEnumerator SpawnRoutine()
+        {
+            while (true)
+            {
+                var poolObject=_objectPool.GetPoolObject(GetRandomPosition(0, _objectPool.GetPoolLengthOfType()));
+                poolObject.transform.position = new Vector3(GetRandomPosition(0,10), 0, GetRandomPosition(0, 10));
+                yield return new WaitForSeconds(_spawnInterval);
+            }
+        }
+
+        private int GetRandomPosition(int value1,int value2)
+        {
+            return Random.Range(value1, value2);
+        }
+    }
+
+}
