1440fd5db719e04a928f85b4d01797c6110a81f0
MelihOzdemir <canmelihozdemir@gmail.com>
2021-05-15 20:20:28 +0300

    + added FlyweightDesignPattern

---
+++ Assets/Scripts/3-FlyweightDesignPattern/Player.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DesignPatterns.Flyweight
+{
+    public class Player : MonoBehaviour
+    {
+        [SerializeField] private PlayerData _playerData=null;
+        private float _currentSpeed = 10;
+        private int _currentHp = 100;
+
+        private void Start()
+        {
+           _currentSpeed =_playerData.MaxSpeed;
+        }
+    }
+
+}
---
+++ Assets/Scripts/3-FlyweightDesignPattern/PlayerData.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DesignPatterns.Flyweight
+{
+    [CreateAssetMenu(fileName ="NewPlayerData",menuName ="Create Player Data")]
+    public class PlayerData : ScriptableObject
+    {
+        private float _maxSpeed = 50f;
+        private float _attackRange = 40f;
+        private float _attackInterval = 3f;
+        private float _maxHp = 100f;
+
+        public float MaxSpeed => _maxSpeed;
+        public float AttackRange => _attackRange;
+        public float AttackInterval => _attackInterval;
+        public float MaxHp => _maxHp;
+
+    }
+
+}
